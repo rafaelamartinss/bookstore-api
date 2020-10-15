1381aea03bdb0a15a37503fe85a1138056d49d37
rafaleal <rafaelalealmartinss@gmail.com>
2020-10-15 17:44:07 -0300

    passport package and users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthorsModule } from './authors/authors.module';
 import { CategoriesModule } from './categories/categories.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthorsModule, CategoriesModule]
+  imports: [AuthorsModule, CategoriesModule, AuthModule, UsersModule]
 })
 export class AppModule {}
