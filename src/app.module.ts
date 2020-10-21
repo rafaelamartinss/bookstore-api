8e5dee40b71f67da11f3e99a19c9bfeac4242217
rafaleal <rafaelalealmartinss@gmail.com>
2020-10-21 18:24:37 -0300

    add basic AppController to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { AuthorsModule } from './authors/authors.module';
 import { CategoriesModule } from './categories/categories.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [AuthorsModule, CategoriesModule, AuthModule, UsersModule]
+  imports: [AuthorsModule, CategoriesModule, AuthModule, UsersModule],
+  controllers: [AppController]
 })
 export class AppModule {}
