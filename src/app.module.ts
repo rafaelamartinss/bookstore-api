9785c08c63ebf60084c8b6964b420019d0ceacca
rafaleal <rafaelalealmartinss@gmail.com>
2020-10-28 22:37:17 -0300

    basic typeORM integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,19 @@ import { CategoriesModule } from './categories/categories.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Connection } from "typeorm";
 
 @Module({
-  imports: [AuthorsModule, CategoriesModule, AuthModule, UsersModule],
+  imports: [
+    AuthorsModule, 
+    CategoriesModule, 
+    AuthModule, 
+    UsersModule,
+    TypeOrmModule.forRoot()
+  ],
   controllers: [AppController]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
